49b2f0b7f8131c5fa726ae2d02455b7e4dd5cf38
Rosyid Haryadi <rosyid_haryadi@protonmail.com>
2025-09-11 13:26:42 +0700

    modal

--- app/Views/transaction/pay.php
+++ app/Views/transaction/pay.php
@@ -27,14 +27,25 @@
         >
     </div>
     <button
-            type="submit"
             class="btn w-100 text-white fw-bold py-2 mb-3"
             style="background-color: #dc3545; border: none; border-radius: 4px;"
+            data-bs-toggle="modal" data-bs-target="#confirmModal" aria-hidden="true"
     >
         Bayar
     </button>
 </div>
 
+<div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="text-center mt-5 mb-5""><img src="<?= base_url('/img/Logo.png') ?>" alt="Logo" class="logo"></div>
+                <div class="text-center mb-3">Beli <?= $service['service_name'] ?> senilai</div>
+                <div class="text-center"><h4>Rp <?= number_format($service['service_tariff'], 0, ',', '.') ?> ?</h4></div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?= $this->endSection(); ?>
 
